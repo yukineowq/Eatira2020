800c4f6ed3d1dbadf8bb75b1fb74f36153e3b6d9
Yuki Neo <yuki.neo34@gmail.com>
2020-05-20 01:03:30 +0800

    Updated landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -33,26 +33,26 @@ const HomepageHeading = ({ mobile }) => (
   <Container text>
     <Header
       as="h1"
-      content="Imagine-a-Company"
+      content="eat() at where you love"
       inverted
       style={{
         fontSize: mobile ? "2em" : "4em",
         fontWeight: "normal",
-        marginBottom: 0,
+        marginBottom: 1,
         marginTop: mobile ? "1.5em" : "3em",
       }}
     />
     <Header
-      as="h2"
-      content="Do whatever you want when you want to."
+      as="h3"
+      content="Singapore's top food social media application to share your food passion."
       inverted
       style={{
-        fontSize: mobile ? "1.5em" : "1.7em",
+        fontSize: mobile ? "1.0em" : "1.3em",
         fontWeight: "normal",
         marginTop: mobile ? "0.5em" : "1.5em",
       }}
     />
-    <Button primary size="huge">
+    <Button secondary size="huge">
       Get Started
       <Icon name="right arrow" />
     </Button>
@@ -89,6 +89,7 @@ class DesktopContainer extends Component {
             textAlign="center"
             style={{ minHeight: 700, padding: "1em 0em" }}
             vertical
+            color="black" // top first hero segment background color
           >
             <Menu
               fixed={fixed ? "top" : null}
@@ -98,20 +99,24 @@ class DesktopContainer extends Component {
               size="large"
             >
               <Container>
-                <Menu.Item as="a" active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as="a">Work</Menu.Item>
-                <Menu.Item as="a">Company</Menu.Item>
-                <Menu.Item as="a">Careers</Menu.Item>
+                <Image
+                  src="https://i.imgur.com/q3ekeqj.jpg"
+                  size="tiny"
+                  circular
+                />
+                <br />
+                <Menu.Item as="a">Features</Menu.Item>
+                <Menu.Item as="a">About</Menu.Item>
+                <Menu.Item as="a">Eatira Deals</Menu.Item>
                 <Menu.Item position="right">
                   <Button as="a" inverted={!fixed}>
-                    Log in
+                    Log In
                   </Button>
                   <Button
                     as="a"
                     inverted={!fixed}
-                    primary={fixed}
+                    //primary={fixed}
+                    color="red"
                     style={{ marginLeft: "0.5em" }}
                   >
                     Sign Up
@@ -158,13 +163,13 @@ class MobileContainer extends Component {
           vertical
           visible={sidebarOpened}
         >
-          <Menu.Item as="a" active>
-            Home
-          </Menu.Item>
-          <Menu.Item as="a">Work</Menu.Item>
-          <Menu.Item as="a">Company</Menu.Item>
-          <Menu.Item as="a">Careers</Menu.Item>
-          <Menu.Item as="a">Log in</Menu.Item>
+          <br />
+          <Image src="https://i.imgur.com/q3ekeqj.jpg" size="tiny" circular />
+          <br />
+          <Menu.Item as="a">Features</Menu.Item>
+          <Menu.Item as="a">About</Menu.Item>
+          <Menu.Item as="a">Eatira Deals</Menu.Item>
+          <Menu.Item as="a">Log In</Menu.Item>
           <Menu.Item as="a">Sign Up</Menu.Item>
         </Sidebar>
 
@@ -184,7 +189,12 @@ class MobileContainer extends Component {
                   <Button as="a" inverted>
                     Log in
                   </Button>
-                  <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                  <Button
+                    as="a"
+                    inverted
+                    style={{ marginLeft: "0.5em" }}
+                    color="red"
+                  >
                     Sign Up
                   </Button>
                 </Menu.Item>
@@ -218,134 +228,93 @@ ResponsiveContainer.propTypes = {
 const HomepageLayout = () => (
   <ResponsiveContainer>
     <Segment style={{ padding: "8em 0em" }} vertical>
+      <Header as="h3" textAlign="center" style={{ fontSize: "2em" }}>
+        Why Eatira ?
+      </Header>
+      <br />
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Help Companies and Companions
+          <Grid.Column width={5}>
+            <Image
+              src="https://image.flaticon.com/icons/svg/2082/2082045.svg"
+              size="small"
+              centered
+            />
+            <Header as="h3" textAlign="center" style={{ fontSize: "2em" }}>
+              Plan A Meal
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              We can give your company superpowers to do things that they never
-              thought possible. Let us delight your customers and empower your
-              needs... through pure data analytics.
-            </p>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              We Make Bananas That Can Dance
+            <Header as="h5" textAlign="center" style={{ fontSize: "1.33em" }}>
+              The most delicious and fastest decision to decide your daily meal
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes that's right, you thought it was the stuff of dreams, but even
-              bananas can be bioengineered.
-            </p>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Image
+              src="https://image.flaticon.com/icons/svg/523/523229.svg"
+              size="small"
+              centered
+            />
+            <Header as="h3" textAlign="center" style={{ fontSize: "2em" }}>
+              Always Free
+            </Header>
+            <Header as="h5" textAlign="center" style={{ fontSize: "1.33em" }}>
+              Of course it's completely free for you, and non-hassle.
+            </Header>
           </Grid.Column>
           <Grid.Column floated="right" width={6}>
             <Image
-              bordered
-              rounded
-              size="large"
-              src="/images/wireframe/white-image.png"
+              src="https://image.flaticon.com/icons/png/512/2933/2933317.png"
+              size="small"
+              centered
             />
+            <Header as="h3" textAlign="center" style={{ fontSize: "2em" }}>
+              Sharing Online
+            </Header>
+            <Header as="h5" textAlign="center" style={{ fontSize: "1.33em" }}>
+              What will we eat next? See your friends, family & colleagues' meal
+              posts!
+            </Header>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign="center">
-            <Button size="huge">Check Them Out</Button>
+            <Button size="huge">Learn More</Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
-    </Segment>
-
-    <Segment style={{ padding: "0em" }} vertical>
-      <Grid celled="internally" columns="equal" stackable>
-        <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              That is what they all say about us
-            </p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: "8em 0em" }} vertical>
-      <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
-        </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
-
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: "3em 0em", textTransform: "uppercase" }}
-        >
-          <a href="#">Case Studies</a>
-        </Divider>
-
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur
-          filler content, but it's really true. It took years of gene splicing
-          and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as="a" size="large">
-          I'm Still Quite Interested
-        </Button>
-      </Container>
     </Segment>
 
     <Segment inverted vertical style={{ padding: "5em 0em" }}>
-      <Container>
+      <Container textAlign="center">
         <Grid divided inverted stackable>
           <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="About" />
+            <Grid.Column width={5}>
+              <Header textAlign="center" inverted as="h3" content="Company" />
               <List link inverted>
-                <List.Item as="a">Sitemap</List.Item>
+                <List.Item as="a">About Us</List.Item>
+                <List.Item as="a">Our Stories</List.Item>
+                <List.Item as="a">Join Us</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <Header textAlign="center" inverted as="h3" content="Help" />
+              <List link inverted>
+                <List.Item as="a">FAQ</List.Item>
                 <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-                <List.Item as="a">Gazebo Plans</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Services" />
-              <List link inverted>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
-              </p>
+            <Grid.Column floated="right" width={6}>
+              <Image
+                src="https://i.imgur.com/q2hXkFV.jpg"
+                size="mini"
+                centered
+                circular
+              />
+              <Header
+                textAlign="center"
+                inverted
+                as="h4"
+                content="©2020 by Eatira All Rights Reserved"
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
